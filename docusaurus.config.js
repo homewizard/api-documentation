c4a0c353244a58766f02d93972f221d944805dda
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2023-11-21 17:03:40 +0100

    Add search by Algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,12 @@ const config = {
                     }
                 ]
             },
+            algolia: {
+                appId: '7BN6NHDFB3',
+                apiKey: '627ac09960f78327bfece9bf65f54d52',
+                indexName: 'api-umentation-homewizard',
+                insights: true
+            },
             footer: {
                 style: 'dark',
                 links: [
