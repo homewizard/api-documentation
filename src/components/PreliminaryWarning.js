20deb6d9a8ec5b91e91e3d351ee2cb6a65165020
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-11-06 16:11:13 +0100

    Update versioning doc (#47)

--- src/components/PreliminaryWarning.js
+++ src/components/PreliminaryWarning.js
@@ -4,7 +4,7 @@ export default function PreliminaryWarning() {
     return (
         <div>
             <Admonition type='warning'>
-                <p>The HomeWizard Energy API V2 documentation is preliminary and subject to change.</p>
+                <p>The HomeWizard Energy API V2 and its documentation is in beta and subject to change.</p>
             </Admonition>
         </div>
     )
