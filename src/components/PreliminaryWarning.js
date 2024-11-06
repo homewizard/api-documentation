175b2fbdeec2638e43eb08ad49c30adc369f392a
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-11-06 16:43:40 +0100

    Make all 'V1' and 'V2' lowercase (#56)

--- src/components/PreliminaryWarning.js
+++ src/components/PreliminaryWarning.js
@@ -4,7 +4,7 @@ export default function PreliminaryWarning() {
     return (
         <div>
             <Admonition type='warning'>
-                <p>The HomeWizard Energy API V2 and its documentation is in beta and subject to change.</p>
+                <p>The HomeWizard Energy API v2 and its documentation is in beta and subject to change.</p>
             </Admonition>
         </div>
     )
