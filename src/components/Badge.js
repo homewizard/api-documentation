55b214b2c00850b3a646a20069929d506069ba4b
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-11-08 14:28:58 +0100

    Clean up version supported batches (#70)
    * Clean up version supported batches
    
    * Extend badges to contain more info
    
    * Extend badges to contain more info
    
    * Fix formatting
    
    * Adjust badges in introduction
    
    * Final touches

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-export default function Badge({ children, color }) {
+import { Icon } from '@iconify/react' // Import the entire Iconify library.
+
+export default function Badge({ children, icon, color, subtext = null, detail = null }) {
     switch (color) {
         case 'hw-orange':
             color = '#ff7100'
             break
         case 'hw-blue':
             color = '#37baff'
+            break
+        case 'hw-grey':
+            color = '#7c7c7c'
             break
         case 'hw-purple':
             color = '#aa00fe'
@@ -22,17 +27,84 @@ export default function Badge({ children, color }) {
     return (
         <span
             style={{
-                display: 'inline-block',
-                backgroundColor: color,
-                borderRadius: '99px',
-                color: '#fff',
-                fontWeight: 'bold',
-                fontSize: '85%',
-                padding: '0.1rem 0.5rem',
-                marginBottom: '1rem'
+                display: 'inline-block'
             }}
         >
-            {children}
+            <span
+                style={{
+                    // display: 'inline-block',
+                    backgroundColor: color,
+                    borderRadius: '99px',
+                    color: '#fff',
+                    padding: '0.1rem 0.5rem',
+                    marginBottom: '0.2rem',
+                    whiteSpace: 'nowrap',
+                    width: 'auto',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start'
+                }}
+            >
+                {/* Left */}
+                <Icon
+                    style={{
+                        margin: '0 auto',
+                        alignSelf: 'center',
+                        marginRight: '0.3rem'
+                    }}
+                    icon={icon}
+                />
+
+                {/* Right */}
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'flex-start',
+                        paddingRight: '0.5rem'
+                    }}
+                >
+                    {/* Right Top */}
+                    <div
+                        style={{
+                            fontWeight: 'bold',
+                            fontSize: '85%'
+                        }}
+                    >
+                        {children}
+                    </div>
+
+                    {/* Right Bottom */}
+                    {subtext && (
+                        <div
+                            style={{
+                                fontSize: '70%',
+                                marginTop: '-0.3rem'
+                            }}
+                        >
+                            {subtext}
+                        </div>
+                    )}
+                </div>
+
+                {/* Detail */}
+                {detail && (
+                    <span
+                        style={{
+                            backgroundColor: 'rgba(255, 255, 255, 0.9)', // Darker background color
+                            borderRadius: '99px',
+                            color: '#111828',
+                            fontWeight: 'bold',
+                            fontSize: '75%',
+                            padding: '0rem 0.3rem',
+                            alignSelf: 'center',
+                            marginRight: '-0.3rem'
+                        }}
+                    >
+                        {detail}
+                    </span>
+                )}
+            </span>
         </span>
     )
 }
