b94dca735d00eab5b52e506278545ea2cbd53a43
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-12-11 16:39:52 +0100

    Improve information about availability of the v2 API

--- src/components/PreliminaryWarning.js
+++ src/components/PreliminaryWarning.js
@@ -4,7 +4,13 @@ export default function PreliminaryWarning() {
     return (
         <div>
             <Admonition type='warning'>
-                <p>The HomeWizard Energy API v2 and its documentation are in beta and subject to change.</p>
+                <p>
+                    The HomeWizard Energy API v2 and its documentation are in beta and subject to change. Complete{' '}
+                    <a href='https://forms.gle/hLuTQcAEDaTJA8FX9' target='_blank'>
+                        this form
+                    </a>{' '}
+                    to join the beta test group.
+                </p>
             </Admonition>
         </div>
     )
